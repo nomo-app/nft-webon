be98a2133d48742d6a6f166207ed9a7117fe2482
nomo-fe <dev2@nomo.app>
2025-08-01 11:34:48 +0200

    perf: introduce rotating provider instance for Polygon

--- src/lib/web3/ethers-providers.ts
+++ src/lib/web3/ethers-providers.ts
@@ -4,14 +4,20 @@ import { nomo, type NomoEvmNetwork } from 'nomo-webon-kit';
 import { ethNetwork, RotatingRpcProvider, rpcUrlsEthereumMainnet } from './rotating-rpc-provider';
 
 export async function getEvmAddress() {
-  // return "0x799021D2fdd2daAE79d61b66012ada83db2Be428";
-  return await nomo.getEvmAddress();
+	//return '0x799021D2fdd2daAE79d61b66012ada83db2Be428';
+	return await nomo.getEvmAddress();
 }
 
-const ethProviderInstance = new RotatingRpcProvider(
-  rpcUrlsEthereumMainnet,
-  ethNetwork,
-);
+const ethProviderInstance = new RotatingRpcProvider(rpcUrlsEthereumMainnet, ethNetwork);
+
+const polygonNetwork = new ethers.Network('polygon', 137n);
+const rpcUrlsPolygon = [
+	'https://rpc.ankr.com/polygon',
+	'https://polygon-rpc.com',
+	'https://matic-mainnet.chainstacklabs.com',
+];
+
+export const polygonProviderInstance = new RotatingRpcProvider(rpcUrlsPolygon, polygonNetwork);
 
 export function getEthersProvider(network: NomoEvmNetwork): ethers.AbstractProvider {
 	if (network === 'zeniq-smart-chain') {
@@ -19,10 +25,7 @@ export function getEthersProvider(network: NomoEvmNetwork): ethers.AbstractProvi
 	} else if (network === 'ethereum') {
 		return ethProviderInstance;
 	} else if (network === 'polygon') {
-		return new ethers.JsonRpcProvider('https://polygon-rpc.com', {
-			name: 'polygon',
-			chainId: 137
-		});
+		return polygonProviderInstance;
 	} else {
 		throw new Error(`Unimplemented network: ${network}`);
 	}
