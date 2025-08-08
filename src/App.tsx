60e8b8069f36921d9db87df2ec6cf945b95e35bd
nomo-fe <dev2@nomo.app>
2025-08-08 11:24:43 +0200

    feat: global state and start page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import { ChainSelect } from "./components/ChainSelect";
+import { GlobalStateProvider } from "./components/global-state-provider";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <GlobalStateProvider>
+      <h1>NFTs</h1>
+      <ChainSelect />
+      <div className="card"></div>
+    </GlobalStateProvider>
+  );
 }
 
-export default App
+export default App;
